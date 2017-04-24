fe433f641d335a48ec932b4a836af596dd12bd32
lyf577 <lyf577@gmail.com>
2017-04-24 23:48:18 +0800

    init

---
+++ app/src/main/java/com/foreverrich/childrensong/MainActivity.java
@@ -0,0 +1,17 @@
+package com.foreverrich.childrensong;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lyf on 2017/4/22.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/foreverrich/childrensong/VideoPlayActivity.java
@@ -0,0 +1,71 @@
+package com.foreverrich.childrensong;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.WindowManager;
+
+import com.foreverrich.childrensong.bean.Video;
+import com.foreverrich.childrensong.widget.UniversalMediaController;
+import com.foreverrich.childrensong.widget.UniversalVideoView;
+
+public class VideoPlayActivity extends AppCompatActivity  implements UniversalVideoView.VideoViewCallback {
+    private String TAG = "VideoPlayActivity";
+    UniversalVideoView mVideoView;
+    UniversalMediaController mMediaController;
+    Video info;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+
+        info = getIntent().getParcelableExtra("video");
+
+        mVideoView = (UniversalVideoView) findViewById(R.id.videoView);
+        mMediaController = (UniversalMediaController) findViewById(R.id.media_controller);
+        mVideoView.setMediaController(mMediaController);
+        mVideoView.setVideoViewCallback(this);
+
+        mVideoView.setVideoPath("http://res1.ubestkid.com/blk/m/054_wawdyey.mp4");
+        mVideoView.requestFocus();
+        mVideoView.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "onDestroy");
+    }
+
+    @Override
+    public void onScaleChange(boolean isFullscreen) {
+
+    }
+
+    @Override
+    public void onPause(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public void onStart(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void onBufferingStart(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public void onBufferingEnd(MediaPlayer mediaPlayer) {
+
+    }
+}
